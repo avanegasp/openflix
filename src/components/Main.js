1779ceed0156fbdbd3025cdf0149a26ea4aaa177
diegozwolf <diegozuluaga88@gmail.com>
2016-11-09 16:18:17 -0500

    create components

---
+++ src/components/Main.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import Button from './Button.js';
+
+class Main extends Component {
+  render() {
+    return (
+    	<div>
+    		<Button>Ingresa</Button>
+    	</div>
+    )            
+  }
+}
+
+export default Main;
---
+++ src/components/Nav.js
@@ -0,0 +1,44 @@
+import React, { Component } from 'react';
+import logo from '../logo.svg';
+import user from '../img/user.png';
+import '../App.css';
+import Dropdown from 'react-dropdown'
+import '../css/dropdown.css';
+
+
+
+const options = [
+		  'Usuario', 'Mi lista', 'Cerrar sesión'
+]
+const defaultOption = options[0]
+
+class Nav extends Component {
+  render() {
+    return (
+      <div >
+        <h3>OpenFlix</h3>
+        <img src={logo} className="App-logo" alt="logo" />
+        <UserNav />
+      </div>
+    );
+  }
+}
+
+
+class UserNav extends Component {
+	_onSelect(){
+		console.log('hola')
+	}
+	render() {
+		return (
+			<div>	
+				<img src={user} alt="img user"/>			
+				<Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />				
+			</div>
+			)
+	}
+}
+
+
+
+export default Nav;

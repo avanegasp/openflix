93987b76d7f068a0f9590f9ae66a554af936c55d
diegozwolf <diegozuluaga88@gmail.com>
2016-11-16 09:24:54 -0500

    hover test in Moviecarousel

--- src/components/MovieCarousel.js
+++ src/components/MovieCarousel.js
@@ -2,6 +2,38 @@ import React, { Component } from 'react';
 import Slider from 'react-slick'
 import MovieInfoTabs from './MovieInfoTabs'
 
+const styles = {
+ outer: {
+    height: '100px',
+    width: '300px',
+    margin: '100px',    
+    cursor: 'pointer',
+    position: 'relative'
+},
+
+normal: {
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'red',
+    opacity: 0
+},
+
+hover: {
+    height: '120%',
+    width: '120%',
+    position: 'absolute',
+    top: 0,
+    bottom: 0,
+    left: 0,
+    right: 0,
+    backgroundColor: 'red',
+    opacity: 1,
+    transition: 'opacity 400ms ease'
+   }
+}
 
 class MovieCarousel extends Component {
   constructor(){
@@ -14,7 +46,7 @@ class MovieCarousel extends Component {
       var settings = {
         arrows: true,
         dots:true,
-        slidesToShow: 5 
+        slidesToShow: 5
       }        
       return (
         <div className='carouselContainer'>
@@ -39,9 +71,29 @@ class MovieCarousel extends Component {
 }
 
 class MovieCard extends Component {
+  constructor(){
+    super()
+    this.state = {
+      hover:false
+    }
+  }
+  onMouseEnterHandle(){
+   this.setState({
+      hover:true
+   })
+  }
+   onMouseLeaveHandler(){
+   this.setState({
+      hover:false
+   })
+  }
   render(){
+  var inner = styles.normal;
+  if(this.state.hover) {
+    inner = styles.hover;
+  }
     return (
-      <div><img src='http://placekitten.com/g/300/100' alt="cat"/></div>
+      <div><img src='http://placekitten.com/g/300/150' alt="cat"/></div>
     )
   }
 }

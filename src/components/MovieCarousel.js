23dcae883593f78c04b3aff4c713f67a02ba58e9
diegozwolf <diegozuluaga88@gmail.com>
2016-11-16 10:05:32 -0500

    fix carousel images in diego branch

--- src/components/MovieCarousel.js
+++ src/components/MovieCarousel.js
@@ -3,38 +3,6 @@ import Slider from 'react-slick'
 //import MovieInfoTabs from './MovieInfoTabs'
 import { getMovieInfo } from '../utils/getMovieInfo'
 
-const styles = {
- outer: {
-    height: '100px',
-    width: '300px',
-    margin: '100px',    
-    cursor: 'pointer',
-    position: 'relative'
-},
-
-normal: {
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'red',
-    opacity: 0
-},
-
-hover: {
-    height: '120%',
-    width: '120%',
-    position: 'absolute',
-    top: 0,
-    bottom: 0,
-    left: 0,
-    right: 0,
-    backgroundColor: 'red',
-    opacity: 1,
-    transition: 'opacity 400ms ease'
-   }
-}
 
 class MovieCarousel extends Component {
   constructor(){
@@ -50,7 +18,6 @@ class MovieCarousel extends Component {
         slidesToShow: 3
       }
       var moviesToMap = this.props.movies       
-
       return (
         <div className='carouselContainer'>
           {
@@ -77,7 +44,6 @@ class MovieCarousel extends Component {
 }
 
 class MovieCard extends Component {
-
   constructor(props){
     super(props)
     this.state = {
@@ -95,17 +61,10 @@ class MovieCard extends Component {
             image:movieImagePath
         }) 
     })
-
   }
   render(){
-  var inner = styles.normal;
-  if(this.state.hover) {
-    inner = styles.hover;
-  }
     return (
-
       <div><img src={`http://image.tmdb.org/t/p/w300/${this.state.image}`} alt={this.state.alt}/></div>
-
     )
   }
 }
